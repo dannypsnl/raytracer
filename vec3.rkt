5fdc0e717137aa7f3605cd999a326e5a220cda1c
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2020-09-02 22:47:17 +0800

    vec3

---
+++ vec3.rkt
@@ -0,0 +1,31 @@
+#lang racket
+
+(struct vec3 (x y z) #:transparent #:mutable)
+(define point3 vec3)
+(define color vec3)
+
+(define (vec-length-squared v)
+  (match v
+    [(vec3 x y z) (+ (* x x) (* y y) (* z z))]))
+(define (vec-length v)
+  (sqrt (vec-length-squared v)))
+
+(define (vec-neg v)
+  (match v
+    [(vec3 x y z)
+     (vec3 (- x) (- y) (- z))]))
+
+(define (vec-+= v v1)
+  (match* (v v1)
+    [((vec3 x y z) (vec3 x1 y1 z1))
+     (set-vec3-x! v (+ x x1))
+     (set-vec3-y! v (+ y y1))
+     (set-vec3-z! v (+ z z1))]))
+(define (vec-*= v t)
+  (match v
+    [(vec3 x y z)
+     (set-vec3-x! v (* x t))
+     (set-vec3-y! v (* y t))
+     (set-vec3-z! v (* z t))]))
+(define (vec-/= v t)
+  (vec-*= v (/ 1 t)))
