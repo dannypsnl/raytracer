bdf71cff021e19a874853a962569d6d0fcbaf444
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2020-09-03 10:40:52 +0800

    use vec3

--- main.rkt
+++ main.rkt
@@ -1,5 +1,7 @@
 #lang racket/base
 
+(require "vec3.rkt")
+
 (module+ main
   (define image-width 256)
   (define image-height 256)
@@ -9,14 +11,10 @@
   (for ([j (in-range (- image-height 1) 0 -1)])
     (eprintf "Scanlines remaining: ~a~n" j)
     (for ([i (in-range 0 image-width)])
-      (define r (/ i (- image-width 1)))
-      (define g (/ j (- image-height 1)))
-      (define b 0.25)
-
-      (define ir (inexact->exact (truncate (* 255.99 r))))
-      (define ig (inexact->exact (truncate (* 255.99 g))))
-      (define ib (inexact->exact (truncate (* 255.99 b))))
-
-      (printf "~a ~a ~a~n" ir ig ib)))
+      (define pixel-color
+        (color (/ i (- image-width 1))
+               (/ j (- image-height 1))
+               0.25))
+      (printf "~a" pixel-color)))
 
   (eprintf "~nDone.~n"))
