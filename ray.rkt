744faf2e751c41ca5d7a0bf8a313b28e7046c814
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2022-01-04 05:26:59 +0800

    convert to typed/racket

--- ray.rkt
+++ ray.rkt
@@ -1,11 +1,21 @@
-#lang racket
+#lang typed/racket
 
-(provide (all-defined-out))
+(provide (struct-out ray)
+         ray-at)
 
-(require "vec3.rkt")
+(require/typed "vec3.rkt"
+               [#:struct vec3 ([x : Flonum]
+                               [y : Flonum]
+                               [z : Flonum])]
+               [vec3-+ (-> vec3 vec3 vec3)]
+               [vec3-* (-> (U vec3 Flonum) (U vec3 Flonum) vec3)])
 
-(struct ray (origin direction) #:transparent)
+(struct ray
+  ([origin : vec3]
+   [direction : vec3])
+  #:transparent)
 
+(: ray-at : ray Float -> vec3)
 (define (ray-at r t)
   (match r
     [(ray origin direction)
