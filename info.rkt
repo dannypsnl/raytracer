3258788b40332cd373d38fd64543d37363ca6b44
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2022-01-04 05:13:03 +0800

    workflows all in one

--- info.rkt
+++ info.rkt
@@ -2,7 +2,6 @@
 (define collection "raytracer")
 (define deps '("base"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
-(define scribblings '(("scribblings/raytracer.scrbl" ())))
 (define pkg-desc "Description Here")
 (define version "0.0")
 (define pkg-authors '(dannypsnl))
